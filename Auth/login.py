a6423be74b08068a160bf0ab0608d22210248449
RamBenitez <ribenitez@up.edu.ph>
2024-06-01 22:51:02 +0800

    Add: log in succesfully and invalid

--- Auth/login.py
+++ Auth/login.py
@@ -1,3 +1,21 @@
 
 def login():
-    print("Login functionality will be implemented here.")
+   #DUMMY LANG
+    valid_username="Alice"
+    valid_password="Alice"
+    valid_name="Alice Guo"
+
+    print("\n-----------LOG IN ---------")
+    username= input("Enter username:")
+    password= input("Enter password:")
+
+    if username == valid_username and password == valid_password:
+        print("\nSuccessfully logged in!")
+        print(f"Welcome, {username}!")
+    else:
+        print("\nInvalid username/password")
+        print("Please try again\n")
+
+
+def successful_login():
+    print("{ nakasign up}")
