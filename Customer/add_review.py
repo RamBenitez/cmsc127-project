c95236b4afc1d1516894c7ffc16e751c7b9e2b08
RamBenitez <ribenitez@up.edu.ph>
2024-06-02 01:56:10 +0800

    Add: add a review for customer

--- Customer/add_review.py
+++ Customer/add_review.py
@@ -1,2 +1,38 @@
 def add_review():
-    print("Add functionality ")
+    while True:
+        print("\n------ADD REVIEW MENU------")
+        print("[1] Add a review on a food item")
+        print("[2] Add a review on a food establishment")
+        print("[3] Back")
+        choice = input("\nEnter your choice: ")
+
+        if choice == '1':
+            add_food_review()
+        elif choice == '2':
+            add_food_establishment_review()
+        elif choice == '3':
+            break
+        else:
+            print("\nInvalid choice. Please try again.")
+
+#Add food review on a food item
+def add_food_review():
+    
+    print("\n-----ADD FOOD REVIEW-----")
+    #QUery to show  all the food items
+    print("FOOD ITEMS....")
+    FoodItemChoice = input("\nChoice: ")
+    FoodRating= input("Rating (1-5):")
+    FoodContent= input("Content")
+    print("\nSuccessully added review of food <FOOD ITEM>\n")
+
+ #add a food establishment review   
+def add_food_establishment_review():
+    print("\n-----ADD FOOD  ESTABLISHMENT REVIEW-----")
+    #QUery to show  all the food establishments
+    print("FOOD ESTABLISHMENTS....")
+    FoodEstablishmentChoice = input("\nChoice: ")
+    EstablishmentRating= input("Rating (1-5):")
+    EstablishmentContent= input("Content")
+    print("\nSuccessully added review of ESTABLISHMENT <FOOD ESTABLSIHMENT>\n")
+    
