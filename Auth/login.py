cc59dce2d15f58de566c01d384fc32d7ca67db6a
aerondain <aerondain@up.edu.ph>
2024-06-03 20:59:49 +0800

    feat: added login functionality

--- Auth/login.py
+++ Auth/login.py
@@ -1,26 +1,41 @@
 from Customer.customer import customer_menu
+from Owner.owner import owner_menu
+from Database import db_util
+import bcrypt # for hashing passwords
 
 def login():
-   #DUMMY LANG ito yong valid credentials for logging in 
-    valid_username="Alice"
-    valid_password="Alice"
-    valid_name="Alice Guo"
 
     print("\n\033[1m\033[94m-----------LOG IN -----------\033[0m")
     username= input("Enter username: ")
     password= input("Enter password: ")
 
-    #successfull login
-    if username == valid_username and password == valid_password:
-        print("\n\033[92mSuccessfully logged in!\033[0m")
-        print(f"\033[92mWelcome, {valid_name}!\033[0m") #dummy palang
-        return valid_name
-     #invalid username or password   
+    query = "SELECT * FROM User WHERE Username = %s"
+    params = (username,)
+    result = db_util.execute_query(query, params, True) # retrieves results that match the username
+
+    if result:
+        stored_password = result[0]['Password'].encode('utf-8')  # Decode to bytes
+        if check_password(stored_password, password):
+            name = result[0]['Name']
+            usertype = result[0]['Usertype']
+            print("\n\033[92mSuccessfully logged in!\033[0m")
+            print(f"\033[92mWelcome, {name}!\033[0m")
+
+            if usertype == 'Customer':
+                print("You are logged in as a Customer.")
+                customer_menu(name)
+            elif usertype == 'Owner':
+                print("You are logged in as an Owner.")
+                owner_menu(name)
+
+            return
+        else:
+            print("\n\033[91mInvalid password. Please try again.\033[0m\n")
+            return None
     else:
-        print("\n\033[91mInvalid username/password\033[0m")
-        print("\033[91mPlease try again\n\033[0m")
+        print("\n\033[91mInvalid username/password. Please try again.\033[0m")
         return None
 
-
-def successful_login(name):
-    customer_menu(name)
+def check_password(stored_password, entered_password):
+    # Check if the entered password matches the stored hashed password
+    return bcrypt.checkpw(entered_password.encode(), stored_password)
