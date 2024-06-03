4c68058fa63771874d4dca8eaa3603709fde845c
Hannah Patricia Samson <hbsamson@up.edu.ph>
2024-06-04 04:14:14 +0800

    feat: add update function

--- Owner/update.py
+++ Owner/update.py
@@ -1,3 +1,6 @@
+from Database import db_util
+from reports import view_food_est, view_food_items_est
+
 def update():
     while True:
         print("\n\033[1m\033[94m------UPDATE MENU------\033[0m") 
@@ -18,23 +21,58 @@ def update():
 
 def update_food_establishment():
     print("\n\033[1m\033[94m------UPDATE A FOOD ESTABLISHMENT------\033[0m")  
-    # Query to get all food establishments
+
     # Print all food establishments
-    print("Food Establishments...")
+    view_food_est()
+
+    print("Pick the ID of the food establishment you want to modify")
     choice = input("Choice: ")
+
     new_name = input("Enter New Food Establishment Name: ")
-    print("\033[92mSuccessfully Updated!\033[0m")
-    # Query to update the food establishment name in the database
+    query = "update Food_Establishment set Food_establishment_name=%s where Food_establishment_id=%s"                                               # Password() for encrpytion
+    params = (new_name, choice) 
+    result = db_util.execute_query(query, params)
+    if result:
+        print("\n\033[92mSuccesfully updated food establishment name to \033[0m\n" + new_name + "\033[0m\n")
+        return True
+    else:
+        print("\n\033[91mFailed to update the food establishment name. Please try again.\033[0m\n")
+        return False
 
 def update_food_item():
     print("\n\033[1m\033[94m------UPDATE A FOOD ITEM------\033[0m")  
     # Query to get all food items of the selected food establishment
     # Print all food items
-    print("Food Items:")
+    view_food_items_est()
+
+    print("Pick the ID of the food you want to modify")
     choice = input("Choice: ")
    
     new_name = input("Enter New Food Item Name: ")
     new_price = input("Enter New Food Item Price: ")
     new_category = input("Enter New Food Item Category: ")
-    print("\033[92mSuccessfully Updated!\033[0m")
-    # Update food item name, price, and category in the database
+    query = "update Food_Item set Food_name=%s, Food_price=%s where Food_id=%s"                                               # Password() for encrpytion
+    params = (new_name, new_price, choice) 
+    result = db_util.execute_query(query, params)
+
+    # Query to delete all food types of food item in food item type
+    delFoodTypes = "delete from Food_Item_Type where Food_id=%s"
+    delFoodTypesParams = (choice, )
+    db_util.execute_query(delFoodTypes, delFoodTypesParams)
+
+    category_list = new_category.split(",")
+
+    addFoodItemType = """
+        INSERT INTO Food_Item_Type (Food_id, Food_item_type)
+        VALUES (%s, %s)
+    """
+    for category in category_list:
+        addFoodItemTypeParams = (choice, category.strip()) 
+        db_util.execute_query(addFoodItemType, addFoodItemTypeParams)
+
+    if result:
+        print("\n\033[92mSuccesfully updated food information\033[0m\n")
+        return True
+    else:
+        print("\n\033[91mFailed to update the food information. Please try again.\033[0m\n")
+        return False
