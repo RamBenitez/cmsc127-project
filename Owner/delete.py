7a0a173b74f3aa54d3b3d3d8cb7af2d8c9944af9
Hannah Patricia Samson <hbsamson@up.edu.ph>
2024-06-04 00:52:11 +0800

    feat: delete food establishment and food items

--- Owner/delete.py
+++ Owner/delete.py
@@ -1,10 +1,11 @@
+from Database import db_util
+
 def delete():
     while True:
         print("\n\033[1m\033[94m------DELETE MENU------\033[0m") 
         print("[1] Delete a food establishment")
         print("[2] Delete a food item")
-        print("[3] Delete a food review")
-        print("[4] Back")
+        print("[3] Back")
         
         choice = input("\nEnter your choice: ")
         
@@ -13,40 +14,75 @@ def delete():
         elif choice == '2':
             delete_food_item()
         elif choice == '3':
-            delete_food_review()
-        elif choice == '4':
             break
         else:
             print("\033[91mInvalid choice. Please try again.\033[0m")
 
 def delete_food_establishment():
     print("\n\033[1m\033[94m------DELETE A FOOD ESTABLISHMENT------\033[0m")  
-    # Query to get all food establishments
-    # Print all food establishments
-    print("List of all Food Establishments...")
-    choice = input("Choice: ")
+    selectAllFoodEst = "select * from Food_Establishment"
+    resultAllFoodEst = db_util.execute_query(selectAllFoodEst, None, True)
+    print("List of Food Establishments")
+    for i in resultAllFoodEst:
+        print(f"{i['Food_establishment_id']}: {i['Food_establishment_name']}")
+
+    print("\nPick the Food Establishment ID you want to delete.")
+    foodEstID = int(input("Choice: "))
+
     # Query to delete the selected food establishment
+    delFoodEst = "delete from Food_Establishment where Food_establishment_id=%s"                                                  
+    delFoodEstParams = (foodEstID) 
+
     # Delete the food establishment from the database
-    print("\033[92mSuccessfully Deleted!\033[0m")
+    delFoodEstResult = db_util.execute_query(delFoodEst, delFoodEstParams)
+
+    if delFoodEstResult:
+        print("\n\033[92mFood establishment successfully deleted!\033[0m\n")
+        return True
+    else:
+        print("\n\033[91mFailed to delete food establishment. Please try again.\033[0m\n")
+        return False
 
 def delete_food_item():
     print("\n\033[1m\033[94m------DELETE A FOOD ITEM------\033[0m")
-    print("List of all Food Establishments...")
-    est_choice = input("Choice: ")  
-    # Query to get all food items of the selected food establishment
-    # Print all food items
-    print("List of all Food Items in :")
-    food_choice = input("Choice: ")
-    # Query to delete the selected food item
-    # Delete the food item from the database
-    print("\033[92mSuccessfully Deleted!\033[0m")
+    selectAllFoodEst = "select * from Food_Establishment"
+    resultAllFoodEst = db_util.execute_query(selectAllFoodEst, None, True)
+    print("List of Food Establishments")
+    for i in resultAllFoodEst:
+        print(f"{i['Food_establishment_id']}: {i['Food_establishment_name']}")
 
-def delete_food_review():
-    print("\n\033[1m------DELETE A FOOD REVIEW------\033[0m")  
-    # Query to get all food reviews
-    # Print all food reviews
-    print("List of all Food Reviews...")
-    choice = input("Choice: ")
-    # Query to delete the selected food review
-    # Delete the food review from the database
-    print("\033[92mSuccessfully Deleted!\033[0m")
+    print("Pick the Food Establishment ID you want to delete.")
+    foodEstID = int(input("Choice: "))
+
+    selectFoodEstName = "SELECT Food_establishment_name FROM Food_Establishment WHERE Food_establishment_id = %s"
+    selectFoodEstNameParams = (foodEstID,)
+    resultselectFoodEstName = db_util.execute_query(selectFoodEstName, selectFoodEstNameParams, True)
+    if resultselectFoodEstName:
+        foodEstName =  resultselectFoodEstName[0]['Food_establishment_name']
+    
+    # Query to get all food items of the selected food establishment
+    selectAllFoodItems = "select * from Food_Establishment natural join Food_Item where Food_establishment_id=%s"
+    selectAllFoodItemsParams = (foodEstID, )
+    resultAllFoodItems = db_util.execute_query(selectAllFoodItems, selectAllFoodItemsParams, True)
+    
+    # Print all food items
+    print("\nList of Food Items in "+ foodEstName)
+    for i in resultAllFoodItems:
+        print(f"{i['Food_id']}: {i['Food_name']}")
+
+    print("\nPick the Food ID you want to delete.")
+    foodID = int(input("Choice: "))
+
+    # Query to delete the selected food item
+    delFoodItem = "delete from Food_Item where Food_id=%s and Food_establishment_id=%s"
+    delFoodItemParams = (foodID, foodEstID) 
+    delFoodItemResult = db_util.execute_query(delFoodItem, delFoodItemParams)
+
+    # Delete the food item from the database
+    if delFoodItemResult:
+        print("\n\033[92mFood item has been successfully deleted!\033[0m\n")
+        return True
+    else:
+        print("\n\033[91mFailed to delete food item. Please try again.\033[0m\n")
+        return False
+
