067c6dbacb7c9b0fa0d5d1d7085c4b57ea5d6e9d
aerondain <aerondain@up.edu.ph>
2024-06-04 05:07:04 +0800

    fix: customer updating no reviews

--- Customer/update_review.py
+++ Customer/update_review.py
@@ -28,7 +28,11 @@ def update_food_review(username):
     """
     params = (username, )
     food_reviews = db_util.execute_query(query, params, fetch=True)
-
+    
+    if not food_reviews:
+        print("\n\033[91mNo food reviews available to update.\033[0m\n")
+        return
+    
     print("CUSTOMER'S FOOD ITEM REVIEWS....")
     for review in food_reviews:
         print(f"Review ID: {review['Food_review_id']}, Rating: {review['Rating']}, Content: {review['Content']}")
@@ -41,8 +45,14 @@ def update_food_review(username):
 
 # Update review of a food establishment
 def update_food_establishment_review(username):
+    food_establishment_reviews = 0
     print("\n\033[1m\033[94m-----UPDATE FOOD ESTABLISHMENT REVIEW-----\033[0m")
     # Query to show all the customer's food establishment reviews
+
+    if not food_establishment_reviews:
+        print("\n\033[91mNo food reviews available to update.\033[0m\n")
+        return
+    
     print("CUSTOMER'S FOOD ESTABLISHMENT REVIEWS....")
     # Customer selects a review to update
     review_choice = input("\nChoice: ")
